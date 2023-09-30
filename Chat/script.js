a3f4d2e82b030c4e333d1f94af54f0a3b8fc5252
kkeizou <84010703+kkeizou@users.noreply.github.com>
2023-09-30 16:35:21 +0900

    Update script.js

--- Chat/script.js
+++ Chat/script.js
@@ -9,39 +9,61 @@ document.addEventListener("DOMContentLoaded", function () {
         const selectedSpeaker = speakerSelect.value;
 
         if (messageText.trim() !== "") {
+            const speakerElement = document.createElement("div");
+            speakerElement.classList.add("speaker");
+            speakerElement.setAttribute("data-speaker", selectedSpeaker);
+            //speakerElement.textContent = `${selectedSpeaker}が言いました:`;
+            chatMessages.appendChild(speakerElement);
+
             const messageElement = document.createElement("div");
             messageElement.classList.add("message");
             messageElement.textContent = messageText;
-            messageElement.style.color = getSpeakerColor(selectedSpeaker); // テキストの色を設定
+            messageElement.style.color = getSpeakerColor(selectedSpeaker);
+            // オフセットを設定
+            let offset = 0;
+            switch (selectedSpeaker) {
+                case "りんごさん":
+                    offset = 10;
+                    break;
+                case "みかんさん":
+                    offset = 20;
+                    break;
+                case "ぶどうさん":
+                    offset = 30;
+                    break;
+                default:
+                    offset = 0;
+            }
+
+            // オフセットを適用
+            messageElement.style.marginLeft = offset + "px";
+
             chatMessages.appendChild(messageElement);
             messageInput.value = "";
             chatMessages.scrollTop = chatMessages.scrollHeight;
 
-            const speakerElement = document.createElement("div");
-            speakerElement.classList.add("speaker");
-            speakerElement.setAttribute("data-speaker", selectedSpeaker);
-            speakerElement.textContent = `${selectedSpeaker}が言いました:`;
-            chatMessages.appendChild(speakerElement);
+
         }
     });
 
+    /* //enterで送信 基本コメントアウト
     messageInput.addEventListener("keydown", function (event) {
         if (event.key === "Enter") {
             sendButton.click();
         }
     });
+    */
 
-    // スピーカーの色を取得する関数
     function getSpeakerColor(speaker) {
         switch (speaker) {
             case "りんごさん":
                 return "red";
             case "みかんさん":
                 return "orange";
-            case "ばななさん":
-                return "yellow";
+            case "ぶどうさん":
+                return "purple";
             default:
-                return "black"; // デフォルトは黒色
+                return "black";
         }
     }
 });
